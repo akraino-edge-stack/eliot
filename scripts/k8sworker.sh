3530ced29151f1778cefe194c6ad338fb58d3d5a
Abhijit Dasgupta <abhijit.das.gupta@huawei.com>
2019-03-05 14:37:42 +0000

    Setup scripts of ELIOT in Ubuntu and X86
    Signed-off-by: Abhijit Dasgupta <abhijit.das.gupta@huawei.com>
    
    Change-Id: I285b07350fb1338da5e06f9816abee6a604faee3

---
+++ scripts/k8sworker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex
+KUBE_VERSION=1.13.0-00
+# Install Kubernetes with Kubeadm
+
+sudo swapoff -a
+sudo apt update
+sudo apt install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+sudo apt update
+sudo apt install -y \
+  kubeadm=${KUBE_VERSION} kubelet=${KUBE_VERSION} 
+
+sudo apt-mark hold kubelet kubeadm
