27e9f5fb9f12ba20b1b45e322d2bcfcae5b0da6f
srinivasan.s.n@huawei.com <srinivasan.s.n@huawei.com>
2019-05-28 10:59:12 +0000

    kubeedge setup script
    Change-Id: I3bf02a3714892208dd7440a7edd5ac63f606d7c0

--- scripts/src/kubeedge_setup.sh
+++ scripts/src/kubeedge_setup.sh
@@ -11,20 +11,26 @@
 take_keedge(){
 
    git clone https://github.com/kubeedge/kubeedge.git $GOPATH/src/github.com/kubeedge/kubeedge
+   source ~/.profile
    cd $GOPATH/src/github.com/kubeedge/kubeedge/keadm
    make
-
+   #kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+   #kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
 }
 
-common_steps="git clone https://github.com/kubeedge/kubeedge.git $GOPATH/src/github.com/kubeedge/kubeedge &&\
-cd $GOPATH/src/github.com/kubeedge/kubeedge/keadm &&\
-make &&\
+source config_kubeedge
+common_steps="git clone https://github.com/kubeedge/kubeedge.git $GOPATH/src/github.com/kubeedge/kubeedge && \
+source ~/.profile && \
+cd $GOPATH/src/github.com/kubeedge/kubeedge/keadm && \
+make && \
 chmod +x kubeedge"
 
 certif_copy="cd /etc/kubeedge &&\
 scp certs.tar.gz"
 
-edge_start="kubeedge join --edgecontrollerip=${nodeip} --edgenodeid=eliot_edge_01"
+edge_start="cd $GOPATH/src/github.com/kubeedge/kubeedge/keadm && \
+chmod +x kubeedge && \
+./kubeedge join --edgecontrollerip=$masternodeip --edgenodeid=eliotedge02 --k8sserverip=$masternodeip:8080"
 
 execute_keedge_controller(){
    cd $GOPATH/src/github.com/kubeedge/kubeedge/keadm
@@ -33,30 +39,38 @@ execute_keedge_controller(){
 }
 
 
+exec_edge(){
+
+   cd $HOME/eliot/scripts/src
+   sshpass -p ${edgenodepassword} scp $HOME/release/eliot/scripts/src/config_kubeedge ${edgenodeusr}@${edgenodeip}:/root
+
+   sshpass -p ${edgenodepassword} ssh ${edgenodeusr}@${edgenodeip} \
+   source config_kubeedge
+   
+   source config_kubeedge
+   sshpass -p ${edgenodepassword} ssh ${edgenodeusr}@${edgenodeip} ${common_steps} < /dev/null
+echo "after common_steps"
+   sshpass -p ${edgenodepassword} scp /etc/kubeedge/certs.tgz ${edgenodeusr}@${edgenodeip}:/etc/kubeedge
+
+   sshpass -p ${edgenodepassword} \
+   ssh ${edgenodeusr}@${edgenodeip} \
+   tar -xvzf /etc/kubeedge/certs.tgz --directory /etc/kubeedge
+   
+   sshpass -p ${edgenodepassword} ssh ${edgenodeusr}@${edgenodeip} ${edge_start} < /dev/null
+}
+
 # start
 
+source config_kubeedge
+#sample
+#return
 take_keedge
 
 execute_keedge_controller
 
-while read line
-do
-    nodeinfo="${line}"
-    nodeusr=$(echo ${nodeinfo} | cut -d"|" -f1)
-    nodeip=$(echo ${nodeinfo} | cut -d"|" -f2)
-    nodepaswd=$(echo ${nodeinfo} | cut -d"|" -f3)
-    masternodeip=$(echo ${nodeinfo} | cut -d"|" -f3)
-
-    sshpass -p ${nodepaswd} ssh ${nodeusr}@${nodeip} ${common_steps} < /dev/null
-    sshpass -p ${nodepaswd} scp /etc/kubeedge/certs.tar.gz ${nodeusr}@${nodepaswd}:/etc/kubeedge
-
-    sshpass -p ${nodepaswd} \
-    tar -xvzf /etc/kubeedge/certs.tgz
-
-    sshpass -p ${nodepaswd} ssh ${nodeusr}@${nodeip} kubeedge join --edgecontrollerip=${masternodeip} --edgenodeid=eliot_edge_01 \
-    --k8sserverip=${masternodeip}:8080
-done < nodelist
-
+#sample
+#execute_edge
+exec_edge
 kubectl get nodes
 
 kubectl create -f deployment.yaml
