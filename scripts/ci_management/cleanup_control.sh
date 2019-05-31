d07bfb2d60715a990d425439e702ceaaefcdce8b
srinivasan.s.n@huawei.com <srinivasan.s.n@huawei.com>
2019-05-31 06:17:35 +0000

    Kubeedge cleanup and code optimisation
    Signed-off-by: srinivasan.s.n@huawei.com <srinivasan.s.n@huawei.com>
    Change-Id: I0f833f9e09b41fb9bb5415c69a228432fa042e70

--- scripts/ci_management/cleanup_control.sh
+++ scripts/ci_management/cleanup_control.sh
@@ -8,22 +8,21 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
+# constants
+
+TESTYAML="testk8s-nginx.yaml"
+
 # start
 
-kubeedge reset
+source ../src/config_kubeedge
 
-reset="kubeedge reset --k8sserverip ${masternodeip}:8080"
+kubectl delete -f testk8s-nginx.yaml
 
-while read line
-do
-    nodeinfo="${line}"
-    nodeusr=$(echo ${nodeinfo} | cut -d"|" -f1)
-    nodeip=$(echo ${nodeinfo} | cut -d"|" -f2)
-    nodepaswd=$(echo ${nodeinfo} | cut -d"|" -f3)
-    masternodeip=$(echo ${nodeinfo} | cut -d"|" -f3)
+sshpass -p ${EDGENODEPASSWORD} \
+scp ${PATH_OF_ELIOTFOLDER}/scripts/ci_management/cleanup_edge.sh ${EDGENODEUSR}@${EDGENODEIP}:$HOME_EDGENODE
 
-    sshpass -p ${nodepaswd} \
-    kubeedge reset --k8sserverip ${masternodeip}:8080
+sshpass -p ${EDGENODEPASSWORD} ssh ${EDGENODEUSR}@${EDGENODEIP} \
+source cleanup_edge.sh
 
-done < nodelist
-
+cd $PATH_OF_ELIOTFOLDER/scripts/ci_management
+source cleanup_master.sh
