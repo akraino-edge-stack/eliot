56139656386ae88df802d20bbc095aeb176cbe68
srinivasan.s.n@huawei.com <srinivasan.s.n@huawei.com>
2019-05-31 06:17:35 +0000

    Kubeedge cleanup and code optimisation
    Signed-off-by: srinivasan.s.n@huawei.com <srinivasan.s.n@huawei.com>
    Change-Id: I0f833f9e09b41fb9bb5415c69a228432fa042e70

---
+++ scripts/ci_management/cleanup_edge.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -ex
+##############################################################################
+# Copyright (c) 2019 Huawei Tech and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# start
+
+source config_kubeedge
+
+cd
+
+if [ -f "test-k8s-nginx.yaml" ]; then
+   kubectl delete -f test-k8s-nginx.yaml
+   sudo rm test-k8s-nginx.yaml
+fi
+
+if [ -d "/root/go/src/github.com/kubeedge/kubeedge/keadm" ]; then
+   cd $GOPATH/src/github.com/kubeedge/kubeedge/keadm
+   ./kubeedge reset --k8sserverip ${masternodeip}:8080
+fi
+
+cd /etc/kubeedge
+
+if [ -f "certs.tgz" ]; then
+   sudo rm -rf certs.tgz
+fi
+
+if [ -d "/etc/kubeedge/ca" ]; then
+   sudo rm -rf /etc/kubeedge/ca
+fi
+
+if [ -d "/etc/kubeedge/certs" ]; then
+   sudo rm -rf /etc/kubeedge/certs
+fi
+
+if [ -d "/root/go/src" ]; then
+   sudo rm -rf /root/go/src
+fi
+
+# stop binaries edge_core
+cd /usr/local/bin
+
+if [ -f "edge_core" ]; then
+   sudo rm edge_core
+fi
+
