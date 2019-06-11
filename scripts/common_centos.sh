a01737eb866c23a76634cbe74b77e69ae1619564
Srinivasan <srinivasan.s.n@huawei.com>
2019-06-11 12:30:49 +0000

    Kubernetes optimised for Centos and Ubuntu
    Signed-off-by: Srinivasan <srinivasan.s.n@huawei.com>
    Change-Id: I0e25f9b47ae6d79f148333d6782f32562943f039

---
+++ scripts/common_centos.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -ex
+
+##############################################################################
+# Copyright (c) 2019 Huawei Tech and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# start
+
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+modprobe br_netfilter
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+
+swapoff -a
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install -y docker-ce
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y kubelet kubeadm kubectl
+
+#sudo reboot
+
+systemctl start docker && systemctl enable docker
+systemctl start kubelet && systemctl enable kubelet
+
+docker info | grep -i cgroup
+
+sed -i 's/cgroup-driver=systemd/cgroup-driver=cgroupfs/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+systemctl daemon-reload
+systemctl restart kubelet
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,6 +18,11 @@
 # sshpass needs to be installed before executing this script.                          #
 ########################################################################################
 
+# constants
+
+OSPLATFORM=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+
+
 show_help()
 {
   echo "The script helps in setting up the ELIOT Toplogy Infrastrucutre"
@@ -36,7 +41,7 @@ show_help()
 # Installing Docker, K8S and Initializing K8S Master
 setup_k8smaster()
 {
-  set -o xtrace
+  #set -o xtrace
   sudo rm -rf ~/.kube
   source common.sh | tee eliotcommon.log
   source k8smaster.sh | tee kubeadm.log
@@ -56,8 +61,8 @@ setup_k8sworkers()
   #SETUP_WORKER_COMMON="cd eliot/scripts && source common.sh"
   SETUP_WORKER="cd eliot/scripts/ && source k8sworker.sh"
 
-  KUBEADM_JOIN=$(grep "kubeadm join " ./kubeadm.log)
-  KUBEADM_JOIN="sudo ${KUBEADM_JOIN}"
+  KUBEADM_JOIN=$(grep "kubeadm join" ./kubeadm.log)
+  KUBEADM_JOIN="${KUBEADM_JOIN}"
 
  # Read all the Worker Node details from nodelist file.
  while read line
@@ -73,6 +78,48 @@ setup_k8sworkers()
 
 }
 
+setup_k8smaster_centos()
+{
+  set -o xtrace
+  sudo rm -rf ~/.kube
+  source common_centos.sh | tee eliotcommon_centos.log
+  source k8smaster_centos.sh | tee kubeadm_centos.log
+
+  # Setup ELIOT Node
+  setup_k8sworkers_centos
+
+  cd cni/calico
+  kubectl apply -f rbac.yaml
+  kubectl apply -f calico.yaml
+}
+
+
+setup_k8sworkers_centos()
+{
+  set -o xtrace
+  # Install Docker on ELIOT Node
+
+  ELIOT_REPO="https://gerrit.akraino.org/r/eliot"
+  SETUP_WORKER_COMMON_CENTOS="sudo rm -rf ~/eliot &&\
+                              git clone ${ELIOT_REPO} &&\
+                              cd eliot/scripts && source common_centos.sh"
+
+  KUBEADM_TOKEN=$(sudo kubeadm token create --print-join-command)
+  KUBEADM_JOIN_CENTOS=${KUBEADM_TOKEN}
+
+ # Read all the Worker Node details from nodelist file.
+ while read line
+ do
+     nodeinfo="${line}"
+     nodeusr=$(echo ${nodeinfo} | cut -d"|" -f1)
+     nodeip=$(echo ${nodeinfo} | cut -d"|" -f2)
+     nodepaswd=$(echo ${nodeinfo} | cut -d"|" -f3)
+     sshpass -p ${nodepaswd} ssh ${nodeusr}@${nodeip} ${SETUP_WORKER_COMMON_CENTOS} < /dev/null
+     #sshpass -p ${nodepaswd} ssh ${nodeusr}@${nodeip} ${SETUP_WORKER_CENTOS} < /dev/null
+     sshpass -p ${nodepaswd} ssh ${nodeusr}@${nodeip} ${KUBEADM_JOIN_CENTOS} < /dev/null
+ done < nodelist
+
+}
 
 #verify kubernetes setup by deploying nginx server.
 
@@ -108,13 +155,16 @@ then
   exit 0
 fi
 
+if [[ $OSPLATFORM = *CentOS* ]]; then
+   setup_k8smaster_centos
+else
+   setup_k8smaster
+fi
 
-setup_k8smaster
 sleep 20
 verify_k8s_status
-
 install_cadvisor_edge
 sleep 10
 install_prometheus
-sleep 5 
+sleep 5
 sudo docker ps | grep prometheus
